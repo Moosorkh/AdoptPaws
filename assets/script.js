91648b72010344c7ae1d8ffe6ab4de83c92521ab
jdeluna06 <jdeluna0672@gmail.com>
2024-07-22 21:33:45 -0700

    Added JS and HTML

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,34 @@
-const productButton= document.querySelectorAll('button');
+const productButton= Array.from(document.querySelectorAll('.add-to-cart'));
 productButton.forEach(button=>{
-function myFunction(){
+button.addEventListener('click', ()=>{
+
     const productId= button.getAttribute('data-product-id');
+    const productName= button.getAttribute('data-product-name');
+    const productPrice= button.getAttribute('data-product-price');
+
     const productObj= {
-        id: productId
+        id: productId, 
+        name: productName,
+        price: productPrice
     }
 
     let cart= JSON.parse(localStorage.getItem('cart')) || [];
     cart.push(productObj);
 
     localStorage.setItem('cart', JSON.stringify(cart));
-}
-productButton.addEventListener('click', myFunction);
+
+});
+   
+
 })
+
+function getItems(){
+    let addedItems= JSON.parse(localStorage.getItem('cart')) || [];
+    addedItems.forEach(item => {
+        console.log(item);
+        
+    });
+    
+} 
+    const shoppingList==document.getElementById('#shopping-list');
+    shoppingList.addEventListener('click', getItems);
